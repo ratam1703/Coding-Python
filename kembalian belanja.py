d9039fb5601ca61b7ff5753234e0f3676ef34266
ratam1703 <rasyadhimas@.com>
2025-02-17 17:41:49 +0700

    tugas sesi 2

---
+++ kembalian belanja.py
@@ -0,0 +1,5 @@
+total_belanja = input("Masukkan total belanja: ")
+uang_dibayarkan = input("Masukkan uang yang dibayarkan: ")
+kembalian = int(uang_dibayarkan) - int(total_belanja)
+
+print("uang kembalian mu adalah Rp.", kembalian,)
---
+++ luas persegi.py
@@ -0,0 +1,4 @@
+sisi_persegi = input("Masukkan sisi persegi: ")
+luas_persegi = int(sisi_persegi) * int(sisi_persegi)
+
+print("luas persegi adalah", luas_persegi, "Cm")
