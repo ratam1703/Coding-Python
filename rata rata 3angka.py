6d54f2f1d2d541afd7f5b15fa1e312e33074ee3f
ratam1703 <rasyadhimas@.com>
2025-02-17 21:33:42 +0700

    tugas sesi 2

---
+++ rata rata 3angka.py
@@ -0,0 +1,6 @@
+angka_pertama = input("Masukkan angka pertama: ")
+angka_kedua = input("Masukkan angka kedua: ")
+angka_ketiga = input("Masukkan angka ketiga: ")
+rumus_ratarata = (int(angka_pertama) + int(angka_kedua) + int(angka_ketiga)) / 3
+
+print("rata ratanya adalah", rumus_ratarata,)
---
+++ segitiga siku siku.py
@@ -0,0 +1,7 @@
+alas_segitiga_siku_siku_a = input("Masukkan alas segitiga siku siku: ")
+tinggi_segitiga_siku_siku_b = input("Masukkan tinggi segitiga siku siku: ")
+sisi_miring_segitiga_siku_siku_c = input("Masukkan sisi miring segitiga siku siku: ")
+luas_segitiga_siku_siku = 1/2 * int(alas_segitiga_siku_siku_a) * int(tinggi_segitiga_siku_siku_b) 
+keliling_segitiga_siku_siku = int(alas_segitiga_siku_siku_a) + int(tinggi_segitiga_siku_siku_b) + int(sisi_miring_segitiga_siku_siku_c)
+print("luas segitiga siku siku adalah", luas_segitiga_siku_siku, "Cm")
+print("keliling segitiga siku siku adalah", keliling_segitiga_siku_siku, "Cm")
